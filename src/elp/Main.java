ac3e03604b3656dd7192d38af4815c166a3936c5
Alfonso Tome <alftome@hotmail.es>
2015-12-15 14:55:42 +0100

    Añadida división

--- src/elp/Main.java
+++ src/elp/Main.java
@@ -1,9 +1,22 @@
 package elp;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		
+		System.out.println("Inrtoduzca el dividendo: ");
+		a = sc.nextInt();
+		System.out.println("Inrtoduzca el d: ");
+		b = sc.nextInt();
+		
+		c = a / b;
+		System.out.println("Resultado = " + c);
+		
+		sc.close();
 	}
 
 }
