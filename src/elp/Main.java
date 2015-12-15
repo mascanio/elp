4d1104cf72727c4c6dc484bdde0f019c0e9cc43b
mascanio <mascanio13@gmail.com>
2015-12-15 16:31:13 +0100

    Añadido test denominador 0

--- src/elp/Main.java
+++ src/elp/Main.java
@@ -7,15 +7,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a, b, c;
-		
+
 		System.out.println("Introduzca el dividendo: ");
 		a = sc.nextInt();
 		System.out.println("Introduzca el divisor: ");
 		b = sc.nextInt();
-		
-		c = a / b;
-		System.out.println("Resultado = " + c);
-		
+
+		if (b != 0) {
+			c = a / b;
+			System.out.println("Resultado = " + c);
+		} else {
+			System.out.println("Error, división por 0");	
+		}
 		sc.close();
 	}
 
